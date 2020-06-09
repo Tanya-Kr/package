aee25b63a2dcc6848b2f0bea20aaca9999c9ea2f
Tanya-Kr <tanya.krug7@gmail.com>
2020-06-09 11:09:30 +0300

    Refactore code Task 5.

--- task5/questionnaire.py
+++ task5/questionnaire.py
@@ -1,6 +1,6 @@
 import glob
+import time
 from contextlib import contextmanager
-from package.task5.timer import Timer
 
 
 QUESTIONS_DIRECTORY_PATH = "Questionnaire"
@@ -11,52 +11,58 @@ TIMING_FILE_PATH = "timing.txt"
 def main():
     files_path_list = glob.glob(QUESTIONS_DIRECTORY_PATH + "/*.txt")
 
-    with file_manager(TIMING_FILE_PATH, "a") as timing_file:
-        with Timer() as timer:
-            filled_questionnaire = get_user_answers(files_path_list)
-            timing_file.write(f"Received answers to questions from {QUESTIONS_DIRECTORY_PATH} "
-                              f"in {str(timer.time_elapsed)} seconds\n")
+    with timing(QUESTIONS_DIRECTORY_PATH):
+        filled_questionnaire = get_user_answers(files_path_list)
 
     create_questionnaire_file(filled_questionnaire)
-
-
-def get_user_answers(files_path_list) -> dict:
-    """Open each questionnaire file. Display all questions line by line in console. Get answer for each question.
-    :return all_answers(dict) with question(key), answer(value)"""
-    with file_manager(TIMING_FILE_PATH, "a+") as timing_file:
-        filled_questionnaire = {}
-        for file_path in files_path_list:
-            with file_manager(file_path, "r") as questionnaire_file:
-                with Timer() as timer:
-
-                    for question in questionnaire_file:
-                        user_answer = input(str(question.replace("\n", " ")))
-                        filled_questionnaire[question.replace("\n", " ")] = user_answer
-
-                    timing_file.write(f"Received answers to questions from {file_path} "
-                                      f"in {str(timer.time_elapsed)} seconds\n")
-        return filled_questionnaire
-
-
-def create_questionnaire_file(filled_questionnaire):
-    """Fill questionnaire file by questions and user answers.
-    :param filled_questionnaire(dict) with question(key), answer(value)"""
-    with file_manager(QUESTIONNAIRE_FILE_PATH) as questionnaire_file:
-        for question in filled_questionnaire:
-            questionnaire_file.write(f"{str(question)} {str(filled_questionnaire[question])}\n")
 
     print("Questionnaire file with <question> : <answer> was created. "
           "File with tracking time answering in each file and the total time "
           "filling out the questionnaire was created.")
 
 
+def get_user_answers(files_path_list: list) -> dict:
+    """Open each questionnaire file. Display all questions line by line in console. Get answer for each question.
+    :param files_path_list(list) with file path.
+    :return all_answers(dict) with question(key), answer(value)."""
+    filled_questionnaire = {}
+    for file_path in files_path_list:
+        with timing(file_path), file_manager(file_path, "r") as questionnaire_file:
+            for question in questionnaire_file:
+                question = question.replace("\n", "")
+                filled_questionnaire[question] = input(question)
+
+    return filled_questionnaire
+
+
+def create_questionnaire_file(filled_questionnaire: dict) -> None:
+    """Fill questionnaire file by questions and user answers.
+    :param filled_questionnaire(dict) with question(key), answer(value)."""
+    with file_manager(QUESTIONNAIRE_FILE_PATH) as questionnaire_file:
+        for question in filled_questionnaire:
+            questionnaire_file.write(f"{question} {filled_questionnaire[question]}\n")
+
+
 @contextmanager
-def file_manager(name, mode="w"):
-    file = open(name, mode)
+def file_manager(file_path: str, mode:str="a+") -> None:
+    """Context manager to open and close file.
+    :param file_path(str), mode(str)"""
+    file = open(file_path, mode)
     try:
         yield file
     finally:
         file.close()
+
+
+@contextmanager
+def timing(path: str) -> None:
+    """Context manager to tracking time of executing some function. Elapsed time is written to a file.
+    :param path(str) of directory or file."""
+    with file_manager(TIMING_FILE_PATH) as timing_file:
+        start = time.time()
+        yield
+        elapsed_time = round((time.time() - start), 1)
+        timing_file.write(f"End {path} in {elapsed_time} seconds\n")
 
 
 if __name__ == '__main__':
